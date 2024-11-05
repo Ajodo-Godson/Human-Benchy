7819feefd20f93eee5d40f6587e300d7810c67c3
Ajodo Godson <godsonajodo2020@gmail.com>
2024-11-05 00:22:31 -0800

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path to where your files are located
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
